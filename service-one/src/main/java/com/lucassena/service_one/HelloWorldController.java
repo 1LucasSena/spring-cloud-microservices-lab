dd22b9c2151932c97b3352c72b89f4ef07201b4f
1LucasSena <lucastsena85@gmail.com>
2026-03-16 20:32:55 -0300

    feat: add service-one with config client and hello-world controller

---
+++ service-one/src/main/java/com/lucassena/service_one/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.lucassena.service_one;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+  @Value("${message:Hello, default message!}")
+  private String message;
+
+  @GetMapping("/message")
+  public String getMessage() {
+    return this.message;
+  }
+}
